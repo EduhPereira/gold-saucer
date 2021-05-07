20a8e45dd13c5bfee26ab0867595c9f4fd464222
Eduardo Pereira <augusto.pereira@protonmail.com>
2021-05-07 10:25:19 -0300

    adding close button and anchor

--- secret-script.js
+++ secret-script.js
@@ -1,7 +1,21 @@
 const input = document.getElementById('password__input');
 const verifyBtn = document.getElementById('secret__btn');
 const resultArea = document.getElementById('result');
+const paragraph = document.getElementById('paragraph');
+const close__secretsBtn = document.getElementById('close__secrets');
 
 verifyBtn.addEventListener('click', () => {
-    if((input.value).toUpperCase() == 'PERFORMATICO'){resultArea.classList.remove('hidden')}
+    if((input.value).toUpperCase() == 'PERFORMATICO'){
+        resultArea.classList.remove('hidden');
+        input.classList.add('hidden');
+        paragraph.classList.add('hidden');
+        verifyBtn.classList.add('hidden');
+    }
+})
+
+close__secretsBtn.addEventListener('click', () => {
+    resultArea.classList.add('hidden');
+    input.classList.remove('hidden');
+    paragraph.classList.remove('hidden');
+    verifyBtn.classList.remove('hidden');
 })
